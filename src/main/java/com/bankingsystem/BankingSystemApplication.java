16364dd2e9a0a3db482cfcdd3635107ae50d48c6
Neha <NEHA-SHARAD.PATIL@CAPGEMINI.COM>
2025-12-17 14:41:18 +0530

    updated

--- src/main/java/com/bankingsystem/BankingSystemApplication.java
+++ src/main/java/com/bankingsystem/BankingSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class BankingSystemApplication {
+    //new
     public static void main(String[] args) {
         SpringApplication.run(BankingSystemApplication.class, args);
     }
